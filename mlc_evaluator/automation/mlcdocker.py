c8da95f0960c8dbc4618292caee7b359aa25e71b
rogthefrog <rogthefrog@gmail.com>
2024-08-21 12:30:33 -0700

    switch back to our custom image; use specified registry instead of the default

--- mlc_evaluator/automation/mlcdocker.py
+++ mlc_evaluator/automation/mlcdocker.py
@@ -16,7 +16,7 @@ def image_uri(
 
 
 def login_cmd(token: str, user: str, registry: str = DEFAULT_REGISTRY) -> str:
-    return f"docker login -u {user} -p {token} {DEFAULT_REGISTRY}"
+    return f"docker login -u {user} -p {token} {registry}"
 
 
 def pull_cmd(image: str, tag: str) -> str:
@@ -29,31 +29,31 @@ def what_is_running_cmd():
 
 def run_cmd(image: str, tag: str) -> str:
     # Ryan's vllm
-    # parts = (
-    #     "docker run",
-    #     "--runtime nvidia",
-    #     "--gpus all",
-    #     # fmt: off
-    #     '--env "HUGGING_FACE_HUB_TOKEN=' + os.getenv("HF_TOKEN") + '"',
-    #     # fmt: on
-    #     "-p 8000:8000",
-    #     "--ipc=host",
-    #     image_uri(image, tag),
-    #     f"--api_key={os.getenv("VLLM_API_KEY", "secret_key")}",
-    #     "--dtype=half"
-    # )
-
-    # default vllm
     parts = (
-        "source /home/admin/.bashrc &&",
-        "docker run --runtime nvidia --gpus all",
-        "-v ~/.cache/huggingface:/root/.cache/huggingface",
+        "docker run",
+        "--runtime nvidia",
+        "--gpus all",
         # fmt: off
-        f'--env "HUGGING_FACE_HUB_TOKEN={os.getenv("HF_TOKEN", "")}"',
+        '--env "HUGGING_FACE_HUB_TOKEN=' + os.getenv("HF_TOKEN") + '"',
+        # fmt: on
         "-p 8000:8000",
         "--ipc=host",
-        "vllm/vllm-openai:latest",
-        "--model mistralai/Mistral-7B-v0.1",
+        image_uri(image, tag),
+        f"--api_key={os.getenv("VLLM_API_KEY", "secret_key")}",
         "--dtype=half"
     )
+
+    # default vllm
+    # parts = (
+    #     "source /home/admin/.bashrc &&",
+    #     "docker run --runtime nvidia --gpus all",
+    #     "-v ~/.cache/huggingface:/root/.cache/huggingface",
+    #     # fmt: off
+    #     f'--env "HUGGING_FACE_HUB_TOKEN={os.getenv("HF_TOKEN", "")}"',
+    #     "-p 8000:8000",
+    #     "--ipc=host",
+    #     "vllm/vllm-openai:latest",
+    #     "--model mistralai/Mistral-7B-v0.1",
+    #     "--dtype=half"
+    # )
     return " ".join(parts)
